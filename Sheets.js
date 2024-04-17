c34662018fbbcf0965aa3c3c716d414875783253
Nils Knittel <go56bog@mytum.de>
2024-04-17 14:41:05 +0200

    verarbeite optionalen Aufdruck

--- Sheets.js
+++ Sheets.js
@@ -1,45 +1,54 @@
 function doPost(e) {
-    /**
-     * Übertrage die Daten, die in dem Formular übermittelt in Google-Sheets
-     * @type {string}
-     */
+    // Definieren der Tabellen
+    let fullSheetName = 'Shirts'; // Name der Tabelle, in die die Daten eingetragen werden sollen
+    let numbersSheetName = 'Nummern' // Name der Tabelle, in die die Nummern eingetragen werden sollen
+    let fullSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(fullSheetName); // Tabelle mit den Daten
+    let numbersSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(numbersSheetName); // Tabelle mit den Nummern
 
-    // Tabelle in der die gesamten Daten gespeichert werden sollen
-    let fullSheetName = 'Shirts';
-    let fullSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(fullSheetName);
-    // Tabelle in der nur die Nummern gespeichert werden sollen. Diese werden benötigt, um zu überprüfen, ob die Nummer bereits vorhanden ist.
-    // Dabei wird eine separate Tabelle verwendet, um die Performance bei der Abfrage der Nummern zu verbessern und Datenschutz zu gewährleisten.
-    let numbersSheetName = 'Nummern'
-    let numbersSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(numbersSheetName);
-
-    // Übermittelte Shirt-Nummer
+    // Nummer aus dem Formular auslesen
     let nummer = e.parameter.nummer;
 
     // Überprüfen, ob die Nummer bereits vorhanden ist
-    let data = numbersSheet.getRange("A:A").getValues(); // Spalte mit den Nummern auslesen
+    let data = numbersSheet.getRange('A:A').getValues(); // Spalte mit den Nummern auslesen
     for (let i = 0; i < data.length; i++) {
-        if (data[i][0] == nummer) {
-            // Nummer bereits vorhanden, daher wird ein Fehler geworfen
+        if (data[i][0] === nummer) {
+            // Nummer wird bereits verwendet
+            // Fehlermeldung zurückgeben
             throw new Error("Die Nummer ist bereits vorhanden.");
         }
     }
 
-    // Daten in die Tabelle eintragen
+    // Daten in die Tabelle einfügen
     let rowData = [];
-    rowData.push(new Date()); // Zeitstempel hinzufügen
-    rowData.push(e.parameter.vorname); // Vorname
-    rowData.push(e.parameter.nachname); // Nachname
-    rowData.push(e.parameter.email); // E-Mail
-    rowData.push(e.parameter.nummer); // Nummer
-    rowData.push(e.parameter.groesse); // Größe
-    rowData.push(e.parameter.geschlecht); // Farbe
+
+    // Zeitstempel hinzufügen
+    rowData.push(new Date());
+
+    // Daten aus dem Formular hinzufügen
+    rowData.push(e.parameter.vorname);
+    rowData.push(e.parameter.nachname);
+    rowData.push(e.parameter.email);
+    rowData.push(e.parameter.nummer);
+    rowData.push(e.parameter.groesse);
+    rowData.push(e.parameter.geschlecht);
+
+    // Überprüfen, ob ein Aufdruck angegeben wurde
+    if (e.parameter.aufdruck === null || e.parameter.aufdruck === '') {
+        rowData.push(e.parameter.nachname); // Standardaufdruck
+    } else {
+        rowData.push(e.parameter.aufdruck); // Wenn ein Aufdruck angegeben wurde, diesen verwenden
+    }
+
+    // Daten in die Tabelle einfügen
     fullSheet.appendRow(rowData);
 
-    // Nummer in die Nummern-Tabelle eintragen
+    // Nummer in die Nummern-Tabelle einfügen
     let numbersData = [];
+    // Nummer hinzufügen
     numbersData.push(e.parameter.nummer);
+    // Nummer in Tabelle einfügen
     numbersSheet.appendRow(numbersData);
 
-    // Rückmeldung, dass die Daten erfolgreich hinzugefügt wurden
+    // Rückmeldung an den Nutzer
     return ContentService.createTextOutput("Daten erfolgreich hinzugefügt.").setMimeType(ContentService.MimeType.TEXT);
 }
