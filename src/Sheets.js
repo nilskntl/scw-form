277d2520ae4c94e737e7b3aef0efe50958a303be
Your Name <you@example.com>
2024-06-15 02:02:22 +0200

    fix: submit shirt form

--- src/Sheets.js
+++ src/Sheets.js
@@ -1,7 +1,7 @@
 const incompleteFormError = new Error('Das Formular wurde nicht vollständig ausgefüllt.');
 const wrongInputError = new Error('Das Formular hat einen falschen Input übermittelt.');
 
-function handleShirt(required, requiredShirt, e) {
+function handleShirt(required, requiredShirt) {
     for (let param in requiredShirt) {
         if (requiredShirt[param] === null || requiredShirt[param] === '') {
             throw incompleteFormError;
@@ -14,7 +14,7 @@ function handleShirt(required, requiredShirt, e) {
     let numbersSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(numbersSheetName); // Tabelle mit den Nummern
 
     // Nummer aus dem Formular auslesen
-    let nummer = requiredShirt.number;
+    let nummer = requiredShirt.shirtNumber;
 
     // Überprüfen, ob die Nummer bereits vorhanden ist
     let data = numbersSheet.getRange('A:A').getValues(); // Spalte mit den Nummern auslesen
@@ -37,17 +37,10 @@ function handleShirt(required, requiredShirt, e) {
     rowData.push(required.lastName); // Nachname
     rowData.push(required.email); // E-Mail Adresse
     rowData.push(requiredShirt.shirtNumber); // Trikotnummer
-    rowData.push(requiredShirt.size); // Größe des Shirts
+    rowData.push(required.size); // Größe des Shirts
     rowData.push(requiredShirt.gender); // Geschlecht
-
-    // Überprüfen, ob ein Aufdruck angegeben wurde
-    if (e.parameter.aufdruck === null || e.parameter.aufdruck === '') {
-        rowData.push(required.nachname); // Standardaufdruck
-    } else {
-        rowData.push(e.parameter.aufdruck); // Wenn ein Aufdruck angegeben wurde, diesen verwenden
-    }
-
-    rowData.push(required.anzahl); // Anzahl der Shirts
+    rowData.push(requiredShirt.shirtPrint); // Wenn ein Aufdruck angegeben wurde, diesen verwenden}
+    rowData.push(required.amount); // Anzahl der Shirts
 
     // Daten in die Tabelle einfügen
     fullSheet.appendRow(rowData);
@@ -55,7 +48,7 @@ function handleShirt(required, requiredShirt, e) {
     // Nummer in die Nummern-Tabelle einfügen
     let numbersData = [];
     // Nummer hinzufügen
-    numbersData.push(requiredShirt.nummer);
+    numbersData.push(requiredShirt.shirtNumber);
     // Nummer in Tabelle einfügen
     numbersSheet.appendRow(numbersData);
 
@@ -63,7 +56,7 @@ function handleShirt(required, requiredShirt, e) {
     return ContentService.createTextOutput("Daten erfolgreich hinzugefügt.").setMimeType(ContentService.MimeType.TEXT);
 }
 
-function handleCap(required, requiredCap, e) {
+function handleCap(required, requiredCap) {
     for (let param in requiredCap) {
         if (requiredCap[param] === null || requiredCap[param] === '') {
             throw incompleteFormError;
@@ -108,6 +101,7 @@ function doPost(e) {
     const requiredShirt = {
         shirtNumber: e.parameter.nummer,
         gender: e.parameter.geschlecht,
+        shirtPrint: e.parameter.aufdruck === null || e.parameter.aufdruck === '' ? e.parameter.nachname : e.parameter.aufdruck,
     }
 
     const requiredCap = {
@@ -120,9 +114,9 @@ function doPost(e) {
         }
     }
     if (required.type === 'badekappe') {
-        return handleCap(required, requiredCap, e);
+        return handleCap(required, requiredCap);
     } else if (required.type === 'shirt') {
-        return handleShirt(required, requiredShirt, e);
+        return handleShirt(required, requiredShirt);
     } else {
         throw wrongInputError;
     }
